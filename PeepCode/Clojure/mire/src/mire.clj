65e0d452b2d41ad9b08e598c9aef3eacc2b4f27c
Andreas Pauley <apauley+github@gmail.com>
2009-12-16 23:54:05 +0200

    Towards using interpreting client commands

---
+++ PeepCode/Clojure/mire/src/mire.clj
@@ -0,0 +1,27 @@
+(ns mire
+  (:use [mire commands])
+  (:use [clojure.contrib server-socket duck-streams]))
+
+(def port 3333)
+
+(defn- print-prompt []
+  (println)
+  (print "> ")
+  (flush))
+
+(defn- mire-handle-client [in out]
+  "Private function to handle client requests"
+  
+  (binding [*in* (reader in) ; Re-assign stdin and stdout
+            *out* (writer out)]
+    (print-prompt)
+    (loop [input (read-line)]
+      (println input)
+      (print-prompt)
+      (recur (read-line)))))
+
+(def server (create-server port mire-handle-client))
+
+(comment
+  (close-server server)
+)
