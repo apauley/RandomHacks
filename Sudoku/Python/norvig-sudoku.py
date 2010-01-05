ae81fb16825c76a28f2acadea6a6f5af4dc7611b
Andreas Pauley <apauley+github@gmail.com>
2010-01-05 10:37:44 +0200

    Sudoku: start exploring the Norvig solution

--- Sudoku/Python/norvig-sudoku.py
+++ Sudoku/Python/norvig-sudoku.py
@@ -7,9 +7,15 @@ rows = 'ABCDEFGHI'
 cols = '123456789'
 digits   = '123456789'
 squares  = cross(rows, cols)
-unitlist = ([cross(rows, c) for c in cols] +
-            [cross(r, cols) for r in rows] +
-            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+
+column_squares = [cross(rows, c) for c in cols]
+row_squares    = [cross(r, cols) for r in rows]
+box_squares    = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+unitlist = (column_squares +
+            row_squares +
+            box_squares)
+
 units = dict((s, [u for u in unitlist if s in u]) 
              for s in squares)
 peers = dict((s, set(s2 for u in units[s] for s2 in u if s2 != s))
